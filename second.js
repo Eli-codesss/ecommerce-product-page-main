540b43b334be77e4645601b5256120635de05d1c
Eli-codesss <111297179+Eli-codesss@users.noreply.github.com>
2023-06-07 14:39:24 -0700

    Stacked images in mobile view

--- second.js
+++ second.js
@@ -14,10 +14,22 @@ const menuBtn = document.getElementById("menu");
 const menuContents = document.getElementById("menu-contents");
 const deleteCart = document.getElementById("deleteCart");
 const menuClose = document.getElementById("menu-close"); 
+const product1 = document.getElementById("product1");
+const product2 = document.getElementById("product2");
+const product3 = document.getElementById("product3");
+const product4 = document.getElementById("product4");
+const container1 = document.getElementById("container1");
+const container2 = document.getElementById("container2");
+const container3 = document.getElementById("container3");
+const container4 = document.getElementById("container4");
 const previousBtn1 = document.getElementById("prev1");
 const previousBtn2 = document.getElementById("prev2");
 const previousBtn3 = document.getElementById("prev3");
 const previousBtn4 = document.getElementById("prev4");
+const nextBtn1 = document.getElementById("nxt1");
+const nextBtn2 = document.getElementById("nxt2");
+const nextBtn3 = document.getElementById("nxt3");
+const nextBtn4 = document.getElementById("nxt4");
 const slide = document.getElementById("images");
 const cartIcon = document.querySelector(".cartmenu");
 const checkout = document.querySelector(".checkout-btn");
@@ -27,6 +39,7 @@ const mainBody = document.querySelector(".mainbody")
 
 let count =  Number(box2.value);
 let totalPrice = 0;
+let counter = 0;
 
     box3.addEventListener("click", function(){
         count = Number(box2.value) + 1;
@@ -59,17 +72,31 @@ let totalPrice = 0;
      checkout.addEventListener("click", function(){
          dropdown.style.display = "none"
          box2.value = 0;
-     })
+     });
 
      menuBtn.addEventListener("touchstart", function(){
          menuContents.style.display = "block"
-     })
+     });
      
      menuClose.addEventListener("touchstart", function(){
          menuContents.style.display = "none"
-     })
+     });
 
-    // if(totalPrice === 0){
-    //     dropdown.innerText = "You have no items in your cart"
-    // }
+     previousBtn1.addEventListener("touchstart", function(){
+        counter -= 1
+        if(counter<0){
+            counter = 0;
+        }
+     });
+
+     nextBtn1.addEventListener("touchstart", function(){
+        counter += 1
+        if(counter>3){
+            counter = 0;
+        }
+     });
+
+     do{
+        product2.style.zIndex = "20"
+     }while(counter===1);
 
